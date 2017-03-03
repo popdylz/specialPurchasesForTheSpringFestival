61edbe7f6ca21154fded82d9082815da5cb81e6f
MU\Administrator <416018952@qq.com>
2017-03-03 21:47:40 +0800

    no message

---
+++ js/data.js
@@ -0,0 +1,167 @@
+var links = [
+    [{
+            idx: "1",
+            link: "https://www.baidu.com/"
+        },
+        {
+            idx: "2",
+            link: "https://www.taobao.com/"
+        },
+        {
+            idx: "3",
+            link: "https://www.jd.com/"
+        },
+        {
+            idx: "4",
+            link: "http://www.suning.com/"
+        }
+    ],
+    [{
+            idx: "1",
+            link: "https://www.baidu.com/"
+        },
+        {
+            idx: "2",
+            link: "https://www.taobao.com/"
+        },
+        {
+            idx: "3",
+            link: "https://www.jd.com/"
+        },
+        {
+            idx: "4",
+            link: "http://www.suning.com/"
+        }
+    ],
+    [{
+            idx: "1",
+            link: "https://www.baidu.com/"
+        },
+        {
+            idx: "2",
+            link: "https://www.taobao.com/"
+        },
+        {
+            idx: "3",
+            link: "https://www.jd.com/"
+        },
+        {
+            idx: "4",
+            link: "http://www.suning.com/"
+        }
+    ],
+    [{
+            idx: "1",
+            link: "https://www.baidu.com/"
+        },
+        {
+            idx: "2",
+            link: "https://www.taobao.com/"
+        },
+        {
+            idx: "3",
+            link: "https://www.jd.com/"
+        },
+        {
+            idx: "4",
+            link: "http://www.suning.com/"
+        }
+    ],
+    [{
+            idx: "1",
+            link: "https://www.baidu.com/"
+        },
+        {
+            idx: "2",
+            link: "https://www.taobao.com/"
+        },
+        {
+            idx: "3",
+            link: "https://www.jd.com/"
+        },
+        {
+            idx: "4",
+            link: "http://www.suning.com/"
+        }
+    ],
+    [{
+            idx: "1",
+            link: "https://www.baidu.com/"
+        },
+        {
+            idx: "2",
+            link: "https://www.taobao.com/"
+        },
+        {
+            idx: "3",
+            link: "https://www.jd.com/"
+        },
+        {
+            idx: "4",
+            link: "http://www.suning.com/"
+        }
+    ],
+    [{
+            idx: "1",
+            link: "https://www.baidu.com/"
+        },
+        {
+            idx: "2",
+            link: "https://www.taobao.com/"
+        },
+        {
+            idx: "3",
+            link: "https://www.jd.com/"
+        },
+        {
+            idx: "4",
+            link: "http://www.suning.com/"
+        }
+    ],
+    [{
+            idx: "1",
+            link: "https://www.baidu.com/"
+        },
+        {
+            idx: "2",
+            link: "https://www.taobao.com/"
+        },
+        {
+            idx: "3",
+            link: "https://www.jd.com/"
+        },
+        {
+            idx: "4",
+            link: "http://www.suning.com/"
+        }
+    ],
+    [{
+            idx: "1",
+            link: "https://www.baidu.com/"
+        },
+        {
+            idx: "2",
+            link: "https://www.taobao.com/"
+        },
+        {
+            idx: "3",
+            link: "https://www.jd.com/"
+        },
+        {
+            idx: "4",
+            link: "http://www.suning.com/"
+        }
+    ]
+];
+$(function() {
+    new Vue({
+        el: "#navBox"
+    });
+    new Vue({
+        el: "#swiperBox",
+        data: {
+            links: links
+        }
+    });
+
+})
